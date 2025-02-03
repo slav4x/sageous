7ef48fe279c0cc8600115370b88dc83a461c6a38
Slava Khripushin <slava.cpa@yandex.ru>
2025-02-03 21:09:19 +0700

    feat: header

--- src/js/main.js
+++ src/js/main.js
@@ -11,4 +11,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const maskedElements = document.querySelectorAll('.masked');
   maskedElements.forEach((item) => new IMask(item, maskOptions));
+
+  const header = document.querySelector('.header');
+  const headerBurgerButton = header.querySelectorAll('.header-burger__button');
+  const headerBurgerPopup = header.querySelector('.header-burger__popup');
+  headerBurgerButton.forEach((button) => {
+    button.addEventListener('click', () => {
+      headerBurgerPopup.classList.toggle('show');
+      header.classList.toggle('show');
+    });
+  });
 });
